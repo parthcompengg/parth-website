95e60d3d773ed2dc22e45e29012bc7cb933b0074
parthshah <shahpa@oregonstate.edu>
2025-04-04 16:01:48 -0700

    re-based the main

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Suspense, lazy } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './layouts/Main'; // fallback for lazy pages
-import './static/css/main.scss'; // All of our styles
+import './static/css/main.css'; // All of our styles
 
 const { PUBLIC_URL } = process.env;
 
